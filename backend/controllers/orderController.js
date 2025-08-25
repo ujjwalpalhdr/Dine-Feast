9257159692694f4909bb936ecbbc2fe221ffe562
UJJWAL PAL <ujjwalpalhdr@gmail.com>
2025-08-25 11:21:05 +0530

    order env

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = process.env.VITE_API_URL;
+  const frontend_url = process.env.FRONTEND_URL;
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
