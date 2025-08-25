84a89b9cf507aefd8176ff948c45a4cec8b3fe00
UJJWAL PAL <ujjwalpalhdr@gmail.com>
2025-08-25 12:06:26 +0530

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url =
+    import.meta.env.VITE_API_URL ||
+    "http://dine-feast-env.eba-7ntepcbg.ap-south-1.elasticbeanstalk.com";
   return (
     <div>
       <ToastContainer />
